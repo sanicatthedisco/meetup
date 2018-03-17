39c6e1796de0b70f6c7ce38f4d8a1cf968b47f5d
sanicatthedisco <jaredlenn90@gmail.com>
2018-03-17 09:13:16 -0400

    Add static folder

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var path = require("path");
+var io = require("socket.io");
+var mysql = require("mysql");
+
+app.use(express.static(path.join(__dirname, "static")));
