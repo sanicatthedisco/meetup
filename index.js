285f461eb09e57af4af08ef96f978fbcf1dd9080
sanicatthedisco <jaredlenn90@gmail.com>
2018-03-17 09:16:43 -0400

    index.js complete

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ var io = require("socket.io");
 var mysql = require("mysql");
 
 app.use(express.static(path.join(__dirname, "static")));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html")):
+});
+
+http.listen(3000, function () {
+    console.log("Listening on port 3000");
+})
+
