4fdac7c9df7f4d5cf5d431db4d2e0d27f5d117ce
sanicatthedisco <jaredlenn90@gmail.com>
2018-03-17 09:47:16 -0400

    Added login, socket and sql framework

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var path = require("path");
-var io = require("socket.io");
+var io = require("socket.io")(http);
 var mysql = require("mysql");
+
+// init server
 
 app.use(express.static(path.join(__dirname, "static")));
 
@@ -15,3 +17,24 @@ http.listen(3000, function () {
     console.log("Listening on port 3000");
 })
 
+//socket stuff
+
+io.on("connection", function(socket) {
+    console.log("A user has connectd with the id of " + socket.id);
+    socket.on("login request", function(credentials) {
+        credentials[]
+    });
+});
+
+//sql stuff
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to mysql");
+});
--- static/client.js
+++ static/client.js
@@ -1,1 +1,8 @@
 var socket = io();
+var userfield = document.getElementById("user");
+var passfield = document.getElementById("password");
+
+
+function submitLogin() {
+    socket.emit("login request", {username: userfield.value, password: passfield.value});
+}
